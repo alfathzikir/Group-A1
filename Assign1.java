419edc14753681a22f31dc63cfe503420a02d844
HAMZAHSPC\Hamzah <hamzah.umar@ucalgary.ca>
2018-03-09 19:24:14 -0700

    This version takes a directory as a command line argument and finds all java files within that directory. Each file is converted into a string and parsed into an AST. The declarations counts and reference counts are incomplete.

--- Assign1.java
+++ Assign1.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import org.eclipse.jdt.core.dom.AST;
@@ -7,11 +11,20 @@ import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
  
-public class Test {
-	public static void main(String args[]){
-		ASTParser parser = ASTParser.newParser(AST.JLS3);
-		parser.setSource("public class A { int i = 9;  \n int j; \n ArrayList<Integer> al = new ArrayList<Integer>();j=1000; }".toCharArray());
-		//parser.setSource("/*abc*/".toCharArray());
+public class Assign1 {
+	
+	public static void main(String[] args) throws IOException {
+		String directory = args[0];
+		String type = args[1];
+		
+		System.out.println(directory);
+		parseFilesInDir(directory);
+	}
+	
+	public static void parse(String str){
+		ASTParser parser = ASTParser.newParser(AST.JLS9);
+		//parser.setSource("public class A { int i = 9;  \n int j; \n ArrayList<Integer> al = new ArrayList<Integer>();j=1000; }".toCharArray());
+		parser.setSource(str.toCharArray());
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
 		//ASTNode node = parser.createAST(null);
  
@@ -38,4 +51,37 @@ public class Test {
  
 		});
 	}
+	
+	public static String readFileToString(String filePath) throws IOException {
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+ 
+		char[] buf = new char[10];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+ 
+		reader.close();
+ 
+		return  fileData.toString();	
+	}
+	
+	public static void parseFilesInDir(String directory) throws IOException{
+		File root = new File(directory);
+		//System.out.println(root.listFiles());
+		File[] files = root.listFiles();
+		String filePath = null;
+ 
+		 for (File f : files ) {
+			 filePath = f.getAbsolutePath();
+			 if(f.isFile()){
+				 System.out.println(filePath);
+				 parse(readFileToString(filePath));
+			 }
+		 }
+	}
+ 
 }
